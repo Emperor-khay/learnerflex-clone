6132602509b5a1762a797510bbf6824dd77e43c1
DioChuks <diochuks65@gmail.com>
2024-09-02 19:59:07 +0100

    feat: market_access to default false

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->enum('vendor_status', array_map(fn($case) => $case->value, VendorStatusEnum::cases()))
                     ->default(VendorStatusEnum::DOWN->value);
             $table->string('otp')->nullable();
-            $table->boolean('market_access')->default(1);
+            $table->boolean('market_access')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
